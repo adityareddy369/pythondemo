38dd6a83c5f75c60b7dbb2e308da41b64b8d448c
ADITYA REDDY TATIPARTHI <adityareddyb.tech2@gmail.com>
2021-05-31 13:23:15 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+
+
+APP = Flask(__name__,template_folder='templates')
+MODEL = pickle.load(open('model.pkl', 'rb'))
